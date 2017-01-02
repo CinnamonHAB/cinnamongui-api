7fdc6f4e1292a029a20a74e7bd571e2af325e606
Mak Krnic <mak@makkrnic.com>
2017-01-02 22:21:04 +0100

    hard-code floorplan and domain
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

---
+++ app/controllers/floorplans_controller.rb
@@ -0,0 +1,21 @@
+class FloorplansController < ApplicationController
+  before_action :set_floorplan, only: [:show, :update, :destroy]
+
+  # GET /floorplans
+  def index
+    @floorplans = Floorplan.all
+
+    render json: @floorplans
+  end
+
+  # GET /floorplans/1
+  def show
+    render json: @floorplan, include: { domain: { include:  { predicates: { include: :predicate_params } } } }
+  end
+
+  private
+
+    def set_floorplan
+      @floorplan = Floorplan.find(params[:id])
+    end
+end
