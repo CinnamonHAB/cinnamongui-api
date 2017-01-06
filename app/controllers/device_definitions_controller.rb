8936adbf6e25edb9c00ed7f2c5a1b70334b04565
Mak Krnic <mak@makkrnic.com>
2017-01-06 21:48:50 +0100

    saving devices
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

---
+++ app/controllers/device_definitions_controller.rb
@@ -0,0 +1,48 @@
+class DeviceDefinitionsController < ApplicationController
+  append_before_action :load_floorplan
+  append_before_action :load_problem
+
+  def show
+    @device_definition = @problem.device_definitions.find params[:id]
+    render json: @device_definition, include: {
+      predicate: {
+        include: :predicate_params
+      }
+    }
+  end
+
+  def create
+    @device_definition = @problem.device_definitions.build device_definition_params
+    @device_definition.save!
+
+    @floorplan_object = @device_definition.build_floorplan_object floorplan_object_params
+    @floorplan_object.save!
+
+    render json: @device_definition, include: {
+      predicate: {
+        include: :predicate_params
+      }
+    }
+  end
+
+  def update
+  end
+
+  private
+
+    def device_definition_params
+      params.require(:device_definition).permit :name, :predicate_id
+    end
+
+    def floorplan_object_params
+      params.require(:device_definition).require(:floorplan_object).permit(:angle, :background_color, :fill, :height, :width, :left, :top, :opacity, :scale_x, :scale_y)
+    end
+
+    def load_floorplan
+      @floorplan = Floorplan.find(params[:floorplan_id])
+    end
+
+    def load_problem
+      @problem = @floorplan.problem
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :floorplan_objects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :floorplans, only: [:index, :show]
+  resources :floorplans, only: [:index, :show] do
+    resources :problems, only: :none do
+      resources :device_definitions
+    end
+  end
 end
