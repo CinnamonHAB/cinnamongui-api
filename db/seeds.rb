2dfe7aa617bc42d333fa8498bc31c0e87f9b3ed6
Mak Krnic <mak@makkrnic.com>
2017-01-07 02:41:11 +0100

    improve seed data
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-FloorplanObject.create!([
-  {angle: 0.0, background_color: nil, fill: "blue", height: 50.0, width: 50.0, left: 200.0, top: 200.0, opacity: 1.0, scale_x: nil, scale_y: nil, type: nil},
-  {angle: 0.0, background_color: nil, fill: "blue", height: 80.0, width: 80.0, left: 300.0, top: 300.0, opacity: 1.0, scale_x: 2, scale_y: 2, type: nil}
-])
-
 domain = Domain.create!({name: "default_domain"})
 
-domain.floorplans.create!
+fp = domain.floorplans.create!
 
 pred = {}
 pred[:lamp] = domain.predicates.create!({keyword: "LAMP", predicate_type: :device})
@@ -21,3 +16,5 @@ pred[:affects].predicate_params.create!({name: "lamp", param_type: "LAMP"})
 pred[:group].predicate_params.create!({name: "g", param_type: nil})
 pred[:in].predicate_params.create!({name: "l", param_type: "LAMP"})
 pred[:in].predicate_params.create!({name: "g", param_type: "GROUP"})
+
+problem = fp.create_problem name: 'default_problem'
